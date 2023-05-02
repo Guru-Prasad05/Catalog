ff1fe9f29de6caae69b7d4fc537985d06519b9c2
Guru-Prasad05 <113578899+Guru-Prasad05@users.noreply.github.com>
2023-05-02 20:14:52 +0530

    Catalog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import Carousel from "./Carousel";
+
+const catalogData = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80",
+    title: "Chicago",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1514565131-fce0801e5785?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=956&q=80",
+    title: "New Jersy",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1543731068-7e0f5beff43a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Maldives",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1493375366763-3ed5e0e6d8ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=906&q=80",
+    title: "Sydney",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1596422846543-75c6fc197f07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=864&q=80",
+    title: "Malaysia",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    title: "Brazil",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 7,
+    image:
+      "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=796&q=80",
+    title: "Rome",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+  {
+    id: 8,
+    image:
+      "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80",
+    title: "Paris",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, vel commodi
+  fuga natus mollitia assumenda quia molestias maxime magni placeat doloremque
+  quo quas similique, eveniet iure magnam laborum fugit eligendi eos
+  aspernatur voluptatibus qui ad cumque porro! Quae distinctio quisquam, id
+  harum rem incidunt impedit magni nostrum. Quidem eos impedit quae,
+  consequuntur error id explicabo, vero, accusamus fuga possimus dolorum.`,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Carousel data={catalogData} />
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Card,
+  CardMedia,
+  Grid,
+  IconButton,
+  Box,
+  Typography,
+  Avatar,
+} from "@mui/material";
+
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+
+const Carousel = ({ data }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    let intervalId = null;
+
+    if (isPlaying) {
+      intervalId = setInterval(() => {
+        const nextIndex = (currentIndex + 1) % data.length;
+        setCurrentIndex(nextIndex);
+      }, 3000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isPlaying, currentIndex, data.length]);
+
+  const handleNextClick = () => {
+    const box = document.querySelector(".box");
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft + 150;
+    const nextIndex = (currentIndex + 1) % data.length;
+    setCurrentIndex(nextIndex);
+    setIsPlaying(false);
+    currentIndex === data.length - 1 &&
+      (box.scrollLeft = box.scrollLeft - width);
+  };
+
+  const handlePrevClick = () => {
+    const box = document.querySelector(".box");
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft - 150;
+    const prevIndex = (currentIndex + data.length - 1) % data.length;
+    setCurrentIndex(prevIndex);
+    setIsPlaying(false);
+    currentIndex === 0 && (box.scrollLeft = box.scrollLeft + width);
+  };
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+    setIsPlaying(false);
+  };
+
+  const handlePlayPauseClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div>
+      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+        <Grid item sx={{ display: "flex" }}>
+          <Card
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "700px",
+              borderRadius: "30px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="500"
+              image={data[currentIndex].image}
+            />
+          </Card>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "400px",
+              ml: "10px",
+              bgcolor: "transparent",
+              p: "10px",
+              borderRadius: "5px",
+            }}
+          >
+            <Typography variant="h2" sx={{ color: "#707070" }}>
+              {data[currentIndex].title}
+            </Typography>
+            <Typography
+              variant="caption"
+              sx={{ color: "#00000029", textAlign: "left" }}
+            >
+              {data[currentIndex].description}
+            </Typography>
+          </Box>
+        </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <div
+            className="box"
+            style={{
+              position: "relative",
+              width: "600px",
+              marginTop: "20px",
+              marginLeft: "250px",
+              display: "flex",
+              overflowX: "hidden",
+              scrollBehavior: "smooth",
+              zIndex: "1",
+            }}
+          >
+            {data.map((data, index) => (
+              <Grid item key={index}>
+                <Button
+                  sx={{ width: "150px" }}
+                  onClick={() => handleIndicatorClick(index)}
+                >
+                  <CardMedia
+                    component="img"
+                    height="90"
+                    image={data.image}
+                    sx={[
+                      { filter: "grayscale(1)", borderRadius: "20px" },
+                      currentIndex === index && {
+                        filter: "grayscale(0)",
+                      },
+                    ]}
+                  />
+                </Button>
+              </Grid>
+            ))}
+          </div>
+          <Box
+            sx={{
+              position: "absolute",
+              display: "flex",
+              width: "750px",
+              transform: "translate(23.5%,50%)",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              sx={{ fontSize: "30px", fontWeight: "600" }}
+              onClick={handlePrevClick}
+            >
+              &lt;
+            </Button>
+            <Button
+              sx={{ fontSize: "30px", fontWeight: "600" }}
+              onClick={handleNextClick}
+            >
+              &gt;
+            </Button>
+          </Box>
+          <Avatar
+            sx={{
+              bgcolor: "#25BEDA",
+              display: "flex",
+              justifyContent: "center",
+              left: "200px",
+              top: "50px",
+            }}
+          >
+            <IconButton
+              sx={{ color: "white", width: "30px" }}
+              onClick={handlePlayPauseClick}
+            >
+              {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+            </IconButton>
+          </Avatar>
+        </Grid>
+      
+    </div>
+  );
+};
+
+export default Carousel;
